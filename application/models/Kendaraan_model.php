d4b2929b8406848318d0eba1450b7fe75b471359
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2021-07-04 12:11:18 +0700

    Mengubah angka menjadi nama saat export list kendaraan ke excel

--- application/models/Kendaraan_model.php
+++ application/models/Kendaraan_model.php
@@ -23,14 +23,16 @@ class Kendaraan_model extends CI_Model
     function get_all($status)
     {
         if(!$status) {
-            $this->db->select('kendaraan.*, merek.nama_merek as nama_merek, type.nama_type as nama_type,jenis_kendaraan.nama_jenis_kendaraan as nama_jenis');
+            $this->db->select('*');
+            $this->db->join('agen', 'agen.agen_id = kendaraan.agen_id', 'left');
             $this->db->join('merek', 'merek.merek_id = kendaraan.merek_id', 'left');
             $this->db->join('type', 'type.type_id = kendaraan.type_id', 'left');
             $this->db->join('jenis_kendaraan', 'jenis_kendaraan.jenis_kendaraan_id = kendaraan.jenis_kendaraan_id', 'left');
             $this->db->order_by($this->id, $this->order);
             return $this->db->get($this->table)->result();
         } else {
-            $this->db->select('kendaraan.*, merek.nama_merek as nama_merek, type.nama_type as nama_type,jenis_kendaraan.nama_jenis_kendaraan as nama_jenis');
+            $this->db->select('*');
+            $this->db->join('agen', 'agen.agen_id = kendaraan.agen_id', 'left');
             $this->db->join('merek', 'merek.merek_id = kendaraan.merek_id', 'left');
             $this->db->join('type', 'type.type_id = kendaraan.type_id', 'left');
             $this->db->join('jenis_kendaraan', 'jenis_kendaraan.jenis_kendaraan_id = kendaraan.jenis_kendaraan_id', 'left');
